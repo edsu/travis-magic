994b0a5acac560286efb456ae5152ae1e832a86a
Ed Summers <ehs@pobox.com>
2013-12-20 11:53:09 -0500

    added tests for calling file directly

--- test.py
+++ test.py
@@ -2,20 +2,31 @@
 
 import magic
 import unittest
+import subprocess
 
 class MagicTest(unittest.TestCase):
-
-    def test_magic_image(self):
-        self.assertEqual(magic.from_file('data/image.png'), 'PNG image data, 144 x 60, 8-bit/color RGBA, non-interlaced')
 
     def test_magic_mime_image(self):
         self.assertEqual(magic.from_file('data/image.png', mime=True), 'image/png')
 
-    def test_magic_doc(self):
-        self.assertTrue(magic.from_file('data/word.doc').startswith('Composite Document File V2 Document'))
-
     def test_magic_mime_doc(self):
         self.assertEqual(magic.from_file('data/word.doc', mime=True), 'application/msword')
+
+    def test_file_image(self):
+        cmd = ['file', '--mime-type', 'data/image.png']
+        r = subprocess.check_output(['file', '--mime-type', 'data/image.png'])
+        mime_type = r.split(":")[1].strip()
+        self.assertEqual(mime_type, 'image/png')
+
+    def test_file_doc(self):
+        cmd = ['file', '--mime-type', 'data/word.doc']
+        r = subprocess.check_output(['file', '--mime-type', 'data/word.doc']).strip()
+        mime_type = r.split(":")[1].strip()
+        self.assertEqual(mime_type, 'application/msword')
+
+
+
+       
 
 
 if __name__ == "__main__":
