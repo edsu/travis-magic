4c35b4f2b21c08b5df203d988e495331da577853
Ed Summers <ehs@pobox.com>
2013-12-20 10:59:27 -0500

    initial

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import magic
+import unittest
+
+class MagicTest(unittest.TestCase):
+
+    def test_magic(self):
+        self.assertEqual(magic.from_file('image.png'), 'PNG image data, 144 x 60, 8-bit/color RGBA, non-interlaced')
+        self.assertEqual(magic.from_file('image.png', mime=True), 'image/png')
+
+
+if __name__ == "__main__":
+    unittest.main()
